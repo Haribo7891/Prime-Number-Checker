e7914ee8c780e50055d284814aa39bbebf0a8320
Haribo7891 <harry.crank@me.com>
2018-02-04 11:55:05 +0000

    Outlets added to [Labels] and [Button]

--- Prime-Numbers/ViewController.swift
+++ Prime-Numbers/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func isItPrime(_ sender: Any) {
+    }
+    
 }
 
